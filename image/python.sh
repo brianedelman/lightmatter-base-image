3a1d63c917adcec8e1ca6a69ae0d9f7521af5523
Luis Orduz <lo@luord.com>
2021-02-18 11:12:01 -0500

    Upgrade pip so that it uses the newest wheels

--- image/python.sh
+++ image/python.sh
@@ -7,5 +7,6 @@ header "Installing Python..."
 ## Install Python.
 minimal_apt_get_install python python2.7 python3.8 python3.8-dev python3-pip python3-setuptools
 #precache uvloop
+python3.8 -m pip install --upgrade pip
 python3.8 -m pip install wheel
 python3.8 -m pip install uvicorn==0.11.3 gunicorn==20.0.4
